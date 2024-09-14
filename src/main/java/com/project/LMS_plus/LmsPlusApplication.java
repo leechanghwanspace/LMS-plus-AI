9f4d7edc363ffb3c63aceba1bc5e2c85ef60753d
leechanghwanspace <spacecehh@naver.com>
2024-09-14 15:54:30 +0900

    [SET] LMS project

---
+++ src/main/java/com/project/LMS_plus/LmsPlusApplication.java
@@ -0,0 +1,13 @@
+package com.project.LMS_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/LMS_plus/LmsPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.LMS_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
