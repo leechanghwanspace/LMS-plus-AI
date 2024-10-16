359aa58bcef84416ff95c36b92bf3496801f6e3c
승일 <seoung1515@naver.com>
2024-10-16 14:47:14 +0900

    [🚑] Add CORS settings

--- src/main/java/com/project/LMS_plus/controller/CommentController.java
+++ src/main/java/com/project/LMS_plus/controller/CommentController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/comments")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 public class CommentController {
 
