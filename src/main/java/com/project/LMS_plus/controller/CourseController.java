20f4558071c69f733fc228fdf93d725fa5b3cd4a
승일 <seoung1515@naver.com>
2024-10-29 15:07:23 +0900

    refactor(CourseController): CORS 설정

--- src/main/java/com/project/LMS_plus/controller/CourseController.java
+++ src/main/java/com/project/LMS_plus/controller/CourseController.java
@@ -2,23 +2,19 @@ package com.project.LMS_plus.controller;
 
 import com.project.LMS_plus.dto.CourseDetailDTO;
 import com.project.LMS_plus.service.CourseService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequiredArgsConstructor
 @RequestMapping("/api/courses")
 public class CourseController {
 
     private final CourseService courseService;
-
-    @Autowired
-    public CourseController(CourseService courseService) {
-        this.courseService = courseService;
-    }
 
     // 모든 과목 조회
     @GetMapping
