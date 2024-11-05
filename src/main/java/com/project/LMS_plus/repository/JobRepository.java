d99ca3120000468f8e511d320807b8324745dc2b
승일 <seoung1515@naver.com>
2024-11-06 00:43:00 +0900

    feat: 직무 관련 메서드 추가

---
+++ src/main/java/com/project/LMS_plus/repository/JobRepository.java
@@ -0,0 +1,7 @@
+package com.project.LMS_plus.repository;
+
+import com.project.LMS_plus.entity.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobRepository extends JpaRepository<Job, Long> {
+}
