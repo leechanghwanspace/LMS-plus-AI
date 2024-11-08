30ca2776e1f5779da0736dbe120a652a7c50d5b4
SeungIlB <seoung1515@naver.com>
2024-11-08 14:44:56 +0900

    feat: 실행시 부서 자동저장 기능

---
+++ src/main/java/com/project/LMS_plus/config/DepartmentConfig.java
@@ -0,0 +1,25 @@
+package com.project.LMS_plus.config;
+
+import com.project.LMS_plus.entity.Department;
+import com.project.LMS_plus.repository.DepartmentRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DepartmentConfig {
+
+    @Bean
+    CommandLineRunner initDepartmentData(DepartmentRepository departmentRepository) {
+        return args -> {
+            // 소프트웨어공학부 데이터가 없을 때만 추가
+            if (departmentRepository.findAll().isEmpty()) {
+                Department department = new Department(
+                        "소프트웨어공학부",
+                        "소프트웨어공학부엔 게임소프트웨어학과, 정보보호학과, 스마트IT학과가 있습니다."
+                );
+                departmentRepository.save(department);
+            }
+        };
+    }
+}
